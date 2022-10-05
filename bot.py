90aac09ba666c78a984d96316fa0d0bfbb1bc2dd
YarinTheDev <110265000+YarinTheDev@users.noreply.github.com>
2022-10-05 23:26:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,72 @@ async def name(interaction: Interaction):
 async def embed(interaction: Interaction):
     embed=nextcord.Embed(title="embed Title", description="Embed Desc", color=nextcord.Colour.red)
     await interaction.response.send_message(embed=embed)
+@bot.event
+async def on_guild_channel_create(channel):
+    guild = bot.get_guild(955063096450355290)
+    channel1 = discord.utils.get(guild.channels, id=971736823397232670)
+    embed = discord.Embed(
+        title="New Channel Has Been Created!",
+        description=f" Channel: `{str(channel.name)}`\n Channel Mention: <#{str(channel.id)}>",
+        colour=discord.Colour.from_rgb(0, 85, 255)
+    )
+    embed.set_footer(text=f"{guild.name} | Channel System", icon_url=guild.icon_url)
+    embed.set_author(name="Channel System", icon_url=guild.icon_url)
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/872450263171100692/955082464513126430/icon.png")
+    await channel1.send(embed=embed)
 
+@bot.event
+async def on_guild_channel_delete(channel):
+    guild = bot.get_guild(955063096450355290)
+    channel1 = discord.utils.get(guild.channels, id=971736823397232670)
+    embed = discord.Embed(
+        title="Channel Has Been Removed",
+        description=f" Channel: `{str(channel.name)}`",
+        colour=discord.Colour.from_rgb(252, 3, 32)
+    )
+    embed.set_footer(text=f"{guild.name} | Channel System", icon_url=guild.icon_url)
+    embed.set_author(name="Channel System", icon_url=guild.icon_url)
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/872450263171100692/955082464513126430/icon.png")
+    await channel1.send(embed=embed)
+@bot.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, id=971736823397232670)
+    embed = discord.Embed(
+        title="User Left The Server",
+        description=f":icon: User: `{str(member)}`\n:icon: Mention: <@{str(member.id)}>",
+        colour=discord.Colour.from_rgb(252, 3, 48)
+    )
+    embed.set_footer(text=f"{member.guild.name} | Leave System", icon_url=member.guild.icon_url)
+    embed.set_author(name="Leave System", icon_url=member.guild.icon_url)
+    embed.set_thumbnail(url=member.avatar_url)
+    await channel.send(embed=embed)
+@bot.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, id=971736823397232670)
+    embed = discord.Embed(
+        title="User Left The Server",
+        description=f":icon: User: `{str(member)}`\n:icon: Mention: <@{str(member.id)}>",
+        colour=discord.Colour.from_rgb(252, 3, 48)
+    )
+    embed.set_footer(text=f"{member.guild.name} | Leave System", icon_url=member.guild.icon_url)
+    embed.set_author(name="Leave System", icon_url=member.guild.icon_url)
+    embed.set_thumbnail(url=member.avatar_url)
+    await channel.send(embed=embed)
+@bot.event
+async def on_message_delete(message):
+    channel = discord.utils.get(message.guild.channels, id=971736823397232670)
+    embed = discord.Embed(
+        title="New Message Has Been Deleted",
+        description=f":icon: User: `{str(message.author)}`\n:icon: Channel: `{str(message.channel)}`\n:icon: Message: `{str(message.content)}`",
+        colour=discord.Colour.from_rgb(0, 85, 255)
+    )
+    embed.set_footer(text=f"{message.guild.name} | Delete System", icon_url=message.guild.icon_url)
+    embed.set_author(name="Delete System", icon_url=message.guild.icon_url)
+    embed.set_thumbnail(url=message.author.avatar_url)
+    if message.author.bot:
+        pass
+    else:
+        await channel.send(embed=embed)
 
 
 
@@ -67,6 +132,45 @@ async def jamal(interaction: Interaction):
     ### do whatever you want i usually pass
         
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+@bot.command()
+async def invites(ctx):
+
+    totalInvites = 0
+    for i in await ctx.guild.invites():
+        if i.inviter == ctx.author:
+            totalInvites += i.uses
+    await ctx.send(f"**You have `{totalInvites}` invites !**")
+@bot.slash_command(name="verify_setup", description="set verify")
+async def verify_setup(interaction: Interaction):
+    view = verifyB()
+    embed = nextcord.Embed(title="__Verify System - Derei & Yarin Developers__", description="**Click The Button To Be Verified.**")
+    await interaction.response.send_message(embed=embed, view=view)
 bot.run(token)
         
     
